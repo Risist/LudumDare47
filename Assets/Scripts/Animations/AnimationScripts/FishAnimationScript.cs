c3fef6ad57f8a2e85832453d9e25952fb32e2382
Risist <risistt@gmail.com>
2020-10-03 16:24:12 +0200

    fish animations implemented and not black hole

---
+++ Assets/Scripts/Animations/AnimationScripts/FishAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FishAnimationScript", menuName = "Ris/Anim/fishAnimationScript", order = 0)]
+public class FishAnimationScript : AnimationScript
+{
+    public override void InitAnimation(AnimationStateMachine stateMachine)
+    {
+        var inputHolder = stateMachine.GetComponent<InputHolder>();
+
+        var idleState = stateMachine.AddNewStateAsCurrent("Idle");
+        idleState.AddUpdate((s) =>
+        {
+            stateMachine.animator.SetBool("atWalk", inputHolder.atMove);
+        });
+    }
+}
---
+++ Assets/Scripts/Gameplay/PullInto.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullInto : MonoBehaviour
+{
+    public float force;
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.attachedRigidbody)
+            return;
+
+        Vector3 into = transform.position - other.attachedRigidbody.position;
+        other.attachedRigidbody.AddForce(into * force);
+        }
+}
