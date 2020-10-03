04f09610a9ba90f35c6c87852b52ca4a38174ca6
bc <bc@jugglergames.com>
2020-10-03 19:38:24 +0200

    added more shaders and black holeray

---
+++ Assets/Scripts/Effects/BlackHoleRayController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Effects
+{
+    public class BlackHoleRayController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _blackHole;
+        [SerializeField] private GameObject _fish;
+        [SerializeField] private ParticleSystem _particle;
+        [SerializeField] private ParticleSystemRenderer _particleRenderer;
+        [SerializeField, Range(0,100)] private float _zm;
+        [SerializeField] private Material _particleMaterial;
+
+        void Awake()
+        {
+            _particleMaterial = _particleRenderer.material;
+        }
+
+        void Update()
+        {
+            var startPos = _fish.transform.position;
+            transform.position = startPos;
+            var delta = (_blackHole.transform.position -startPos);
+            transform.rotation = Quaternion.LookRotation( delta.normalized);
+
+            var vof = _particle.velocityOverLifetime;
+            vof.zMultiplier = _zm*delta.magnitude;
+
+            var mm = _particle.main;
+            var ffactor = Mathf.InverseLerp(20, 0, delta.magnitude) + 0.2f;
+            mm.simulationSpeed = ffactor;
+
+            _particleMaterial.SetFloat("_InvFade", Mathf.Clamp01(ffactor));
+        }
+
+    }
+}
