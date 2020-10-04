6e68d6e36167a67d7f5cb064804cd93011c0ff03
Risist <risistt@gmail.com>
2020-10-04 13:46:38 +0200

    d

--- Assets/Scripts/Gameplay/PullInto.cs
+++ Assets/Scripts/Gameplay/PullInto.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class PullInto : MonoBehaviour
 {
     public float force;
+    public float sideForce;
     private void OnTriggerStay(Collider other)
     {
         if (!other.attachedRigidbody || other.isTrigger)
             return;
 
         Vector3 into = transform.position - other.attachedRigidbody.position;
-        other.attachedRigidbody.AddForce(into * force);
+        other.attachedRigidbody.AddForce(into * force + new Vector3(-into.z, into.y, into.x)*sideForce );
         }
 }
